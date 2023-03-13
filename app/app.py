7288fa9044a89b44c63135217d1948d3c07e5fdc
Gabriel72220 <123021134+Gabriel72220@users.noreply.github.com>
2023-03-13 20:00:30 -0300

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 import pdfplumber as pdf
 import os
 j=0
-tam=1
-array=["Mestrado","Doutorado"]
-i=[0,0]
-arquivo = open('B:/Documentos/Programacao/Trabalho/novo-arquivo.txt', 'r')
+#22 salgados
+#26 refri
+#18
+tam_salgados=21
+tam_refri=47
+array=["Croassaint misto","coxinha","empada","pastel forno","pao batata frango","pao de batata misto","esfiha mista","esfiha carne","esfiha mistao","esfiha queijo","croassaint queijo","croassaint frango","quiche","enroladinho","doguinho","hamburgue","kibe","coracao","beirute","esfiha frango","almofada","esfiha calabresa","coca lata","coca 250ml","coca 1L","coca 600ml","refri laranja","refri cola","pepsi 1l","pepsi 2l","pepsi lata","pepsi twist","pepsi black","cajuina 250ml","cajuina 1l","cajuina 2l","guarana lata","guarana 1l","fanta 250","fanta 2l","agua","agua com gas","citrus","pepsi 250ml","suco","fanta lata maracuja","fanta lata laranja","guarana kuat 1l","refri guarana"]
+
+i=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+arquivo = open('C:/Users/Usuario/Documents/Extracao_dados_para_excel-main/novo-arquivo.txt', 'r')
 lista = arquivo.read()
 chars = "[],'"
 lista = str(lista).translate(str.maketrans('', '', chars))
@@ -12,7 +17,7 @@ arquivo.close()
 print(len(lista.split()))
 run=True
 while run:
-    if j<=1:
+    if j<=46:
         i[j]=int(lista.split()[j])
         j+=1
     else:
@@ -20,28 +25,48 @@ while run:
 run=True
 resultado=0
 while run:
-    x=input(" 1- Ver Quantidade Total de salgados vendidos\n 2- ver quantidade vendida de cada salgado\n 3- ver salgados vendidos no atacado\n 4- Zerar Tabela\n 0- fechar ")
+    x=input(" 1- Ver Quantidade Total de salgados vendidos\n 2- ver quantidade vendida de cada salgado\n 3- ver total de bebidas vendidas\n 4- ver lista de bebidas vendidas \n 5- Zerar Tabela\n 6- Beirute \n 0- fechar ")
     if int(x)==0:
        run=False
     if int(x)==1:
        j=0
-       while j<=tam:
+       resultado=0
+       while j<=tam_salgados:
            resultado+=int(i[j]) 
            j+=1
+           if j==18:
+              j+=1
        print("Foram Vendidos:",int(resultado) ,"Salgados")
     if int(x)==2:
        j=0
-       while j<=tam:
+       while j<=tam_salgados:
         print(array[j],":",i[j])
         j+=1
+        if j==18:
+          j+=1
+
     if int(x)==3:
-       print("EM desenvolvimento")
+       j=22
+       resultado=0
+       while j<=tam_refri:
+           resultado+=int(i[j]) 
+           j+=1
+       print("Foram Vendidos:",int(resultado) ,"Bebidas")
+    
     if int(x)==4:
+       j=22
+
+       while j<=tam_refri:
+        print(array[j],":",i[j])
+        j+=1
+    if int(x)==5:
        j=0
-       while j<=tam:
+       while j<=47:
          i[j]=0 
          j+=1
-       arquivo = open('B:/Documentos/Programacao/Trabalho/novo-arquivo.txt', 'w')
+       arquivo = open('C:/Users/Usuario/Documents/Extracao_dados_para_excel-main/novo-arquivo.txt', 'w')
        arquivo.write(str(i))
        arquivo.close()
-       
+    
+    if int(x)==6:
+        print(array[18],":",i[18])
--- index.py
+++ index.py
@@ -2,8 +2,8 @@ import pdfplumber as pdf
 import os
 import time
 j=0
-i=[0,0]
-array=["Mestrado","Doutorado"]
+i=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+array=["000001","000002","000003","000004","000005","000008","000009","000010","000011","000012","000013","000014","000016","000018","000019","000020","000021","000022","000023","000025","000098","000099","000052","000053","000054","000055","000056","000057","000058","000059","000060","000061","000062","000063","000064","000065","000066","000067","000068","000069","000070","000071","000072","000073","000149","000150","000151","000152","000103"]
 def buscarpdf(file):
     with pdf.open(file) as tool:
         for p_no,page in enumerate(tool.pages, 1):
@@ -16,43 +16,52 @@ def cacarinformacao(text,array,qtd):
     j=0
     run=True
     b=0
+    aux=0
     tamanho= int (len(text.split()))
     quantidade=0
     while run:
         if array[j] in text.split()[b]:
-            quantidade=int(text.split()[b+1])
-            i[j]=i[j]+quantidade
+          if len(array[j]) == len(text.split()[b]): 
+           run2=True
+           aux=b
+           while run2: 
+            if("UNI" in text.split()[aux]):
+                quantidade=int(text.split()[aux-1])
+                i[j]=i[j]+quantidade
+                run2=False
+            else:
+                aux+=1
             
         if b<tamanho-1:
             b+=1
         else:
             b=0
             j+=1
-        if(j==2):
+        if(j==48):
             run=False
 def exclui():
-    os.remove("B:/Documentos/PDF/2.pdf")
+    os.remove("C:/Users/Usuario/Documents/Extracao_dados_para_excel-main/2.pdf")
 TRUE = True
-
-arquivo = open('B:/Documentos/Programacao/Trabalho/novo-arquivo.txt', 'r')
+j=0
+arquivo = open('novo-arquivo.txt', 'r')
 lista = arquivo.read()
 chars = "[],'"
 lista = str(lista).translate(str.maketrans('', '', chars))
 arquivo.close()
 run1=True
 while run1:
-    if j<=1:
+    if j<=46:
         i[j]=int(lista.split()[j])
         j+=1
     else:
         run1=False
 
 while TRUE:
-    diretorio = 'B:/Documentos/PDF/2.pdf'
+    diretorio = 'C:/Users/Usuario/Documents/Extracao_dados_para_excel-main/2.pdf'
     existe = os.path.exists(diretorio)
     if existe == True:
        time.sleep(2)
-       text=buscarpdf("B:/Documentos/PDF/2.pdf")
+       text=buscarpdf("C:/Users/Usuario/Documents/Extracao_dados_para_excel-main/2.pdf")
        cacarinformacao(text,array,i)
        exclui()
        
@@ -64,5 +73,5 @@ while TRUE:
        chars = "[],'"
        lista = str(lista).translate(str.maketrans('', '', chars))
        arquivo.close()
-       print(lista)
+       
        
